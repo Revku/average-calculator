2b9f4e34eb5ae406014a84171833ae496fa55f3f
TheRevQ <biznes.therevq@gmail.com>
2021-01-24 20:09:14 +0100

    v1.1.0 Version - Add polish translate - Fix float number - Add submit with enter button

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="box">
         <div class="box__header">
-            <h2>{{ appTitle }}</h2>
+            <h2>{{ translate.appTitle }}</h2>
         </div>
 
-        <input type="text" placeholder="New number" class="input">
-        <button @click="newNumber()" class="btn">Add</button>
+        <input type="text" :placeholder="translate.inputPlaceholder" class="input" @keyup.enter="newNumber()">
+        <button @click="newNumber()" class="btn">{{ translate.addButton }}</button>
         <p class="errorbox">{{ errorMessage }}</p>
 
         <p class="box__emptyerror">{{ listStatus }}</p>
@@ -13,7 +13,7 @@
         <div class="box__container">
             <div v-for="number in numbers" :key="number.id" class="box__container__item">
                 <p class="input p">{{ number.v }}</p>
-                <button @click="removeNumber(number.id)" class="btn">Remove</button>
+                <button @click="removeNumber(number.id)" class="btn">{{ translate.removeButton }}</button>
             </div>
         </div>
 
@@ -22,38 +22,49 @@
 </template>
 
 <script>
+import EnglishTranslate from '../locales/en.json'
+import PolishTranslate from '../locales/pl.json'
     export default {
         name: 'Box',
         data: function() {
             return {
-                appTitle: "Average Calculator",
-                listStatus: "List is empty",
-                version: "v1.0.2",
+                translate: EnglishTranslate,
                 errorMessage: '',
+                listStatus: '',
 
                 numbers: [],
                 average: 0,
             }
         },
         methods: {
+            checkTranslate() {
+                if (navigator.language == "pl-PL") {
+                    this.translate = PolishTranslate;
+                } else {
+                    this.translate = EnglishTranslate;
+                }
+            },
             newNumber: function() {
                 const input = document.querySelector('input');
-                const val = input.value;
-                console.log(val);
                 const id = Math.random();
-                if (val.search(",") >= 1) {
-                    this.errorMessage = "Use a period as a comma!";
+                let val = input.value;
+
+                if (val.search(",") >= 0) {
+                    val = val.replace(/,/g, '.');
+                }
+
+                if (!val) {
+                    this.errorMessage = this.translate.errorField;
                 } else if (isNaN(val)) {
-                        this.errorMessage = "Value is not a number!";
-                } else if (!val) {
-                    this.errorMessage = "Field is empty!";
-                } 
-                else {
-                    this.numbers.push({v: parseFloat(val), id: id});
-                    this.listStatus = "Numbers";
+                    this.errorMessage = this.translate.errorNaN;
+                } else {
+                    const number = parseFloat(val).toFixed(2);
+                    this.numbers.push({v: parseFloat(number), id: id});
+                    this.listStatus = this.translate.listStatusNumbers;
                     this.errorMessage = "";
                     this.getAverage();
                 }
+                input.value = '';
                 this.isEmpty();
             },
             removeNumber: function(newID) {
@@ -72,18 +83,21 @@
                     suma = suma + this.numbers[i].v;
                     const average = suma / this.numbers.length;
                     this.average = average.toFixed(2);
-                    console.log(this.average);
                 }
                 this.isEmpty();
             },
             isEmpty: function() {
                 if (this.numbers.length <= 0) {
-                    this.listStatus = "List is empty";
+                    this.listStatus = this.translate.listStatusEmpty;
                     document.querySelector('.box__result').classList.add('none');
                 } else {
                     document.querySelector('.box__result').classList.remove('none');
                 }
             },
+        },
+        mounted() {
+            this.checkTranslate();
+            this.listStatus = this.translate.listStatusEmpty;
         }
     }
 </script>
